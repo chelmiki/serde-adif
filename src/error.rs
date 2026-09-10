aaa023aa7cad5c8b10bf78eb3f70e2e2a86bd7a9
chelmiki <11820534+chelmiki@users.noreply.github.com>
2026-09-10 22:44:57 +0100

    Clean up leftover comments from serde-json tutorial

--- src/error.rs
+++ src/error.rs
@@ -1,27 +1,10 @@
-// Copyright 2018 Serde Developers
-//
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
-
 use serde::{de, ser};
 use std::fmt::{self, Display};
 
 pub type Result<T> = std::result::Result<T, Error>;
 
-// This is a bare-bones implementation. A real library would provide additional
-// information in its error type, for example the line and column at which the
-// error occurred, the byte offset into the input, or the current key being
-// processed.
 #[derive(Debug)]
 pub enum Error {
-    // One or more variants that can be created by data structures through the
-    // `ser::Error` and `de::Error` traits. For example the Serialize impl for
-    // Mutex<T> might return an error because the mutex is poisoned, or the
-    // Deserialize impl for a struct may return an error because a required
-    // field is missing.
     Message(String),
     Eof,
     /// A tag was opened with "<" but no matching ">" was found. Carries a
